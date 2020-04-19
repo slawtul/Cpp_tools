30bd91d1429b34ca315a64142b17f6763a3d2f28
slawtul <slawtul@javablues.org>
2020-04-19 23:18:41 +0200

    Add confirmation question before running

--- src/sort_files/sort_files.cpp
+++ src/sort_files/sort_files.cpp
@@ -9,13 +9,22 @@ namespace fs = std::filesystem;
 
 int main()
 {
-    std::cout << "program: sort_files\n";
     auto current_path = fs::current_path();
+    std::cout << "program: sort_files\n";
+    std::cout << "please confirm you want to run it inside " << current_path << " [y/n]\n";
+
+    char confirm;
+    std::cin >> confirm;
+
+    if (confirm != 'y') {
+        std::cout << "stopped" << "\n";
+        return 0;
+    }
+
     fs::create_directory(current_path / "photos");
     fs::create_directory(current_path / "movies");
 
-    int img_counter = 0;
-    int mov_counter = 0;
+    int f_counter = 0;
     const auto start = std::chrono::high_resolution_clock::now();
     for (const auto &file : fs::directory_iterator(current_path)) {
         if (file.is_directory()) {
@@ -27,25 +36,22 @@ int main()
         if (f_ext.empty() || f_ext == ".") {
             continue;
         }
-        if (has_ext(f_ext, {".jpg", ".png", ".raw", ".mov", ".mp4", ".arw"})) {
-            img_counter++;
+        if (has_ext(f_ext, {".jpg", ".png", ".gif", ".raw", ".arw", ".mov", ".mp4"})) {
             const auto&[year, month] = get_year_and_month(file);
             const auto &quart = std::to_string(quarter(month));
+            const auto
+                res_path = current_path.assign(dest_folder(f_ext)).append(year).append(quart);
             f_print(f_path, year, month, quart);
-            const auto res_path = current_path
-                .assign(dest_folder(f_ext))
-                .append(year)
-                .append(quart);
             fs::create_directories(current_path.assign(res_path));
             auto f_name = f_path.filename().string();
             std::transform(f_name.begin(), f_name.end(), f_name.begin(), ::tolower);
             fs::rename(f_path, fs::current_path() / res_path / f_name);
+            ++f_counter;
         }
     }
     const auto stop = std::chrono::high_resolution_clock::now();
     const auto duration = duration_cast<std::chrono::milliseconds>(stop - start);
     std::cout << "exec time: " << duration.count() << " ms\n";
-    std::cout << "found: " << img_counter << " images\n";
-    std::cout << "found: " << mov_counter << " movies\n";
+    std::cout << "found: " << f_counter << " files\n";
     return 0;
 }
