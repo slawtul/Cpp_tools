c03b40714289c8d30674f16a953cf7425b51338e
slawtul <slawtul@javablues.org>
2020-06-06 01:43:12 +0200

    Refactoring...

--- Sort_files_run.cpp
+++ Sort_files_run.cpp
@@ -1,11 +1,8 @@
 #include "Sort_files.h"
 
-namespace fs = std::filesystem;
-namespace ch = std::chrono;
-
 int main()
 {
-    auto c_path = fs::current_path();
+    auto c_path = std::filesystem::current_path();
     std::cout << "program: sort_files\n";
     std::cout << "please confirm you want to run it inside "
               << c_path << " [y/n]\n";
@@ -19,14 +16,14 @@ int main()
         return 0;
     }
 
-    const auto start = ch::high_resolution_clock::now();
+    const auto start = std::chrono::high_resolution_clock::now();
     Sort_files sf{c_path};
     sf.start();
-    const auto stop = ch::high_resolution_clock::now();
+    const auto stop = std::chrono::high_resolution_clock::now();
 
     std::cout << "found files: " << sf.count_f() << "\n";
     std::cout << "exec time [ms]: "
-              << duration_cast<ch::milliseconds>(stop - start).count()
+              << duration_cast<std::chrono::milliseconds>(stop - start).count()
               << "\n";
     return 0;
 }
