2b7e02afe95411e27d9ed89c58f5c3daf9bfff6b
slawtul <slawtul@javablues.org>
2020-10-24 23:37:57 +0200

    Add variant and other approach

--- playground_run.cpp
+++ playground_run.cpp
@@ -1,9 +1,28 @@
 #include <memory>
 #include <iostream>
+#include <variant>
+#include <vector>
 #include "playground.h"
 #include "simple_struct.h"
 
-int main() {
+struct A
+{
+  void update()
+  {
+    std::cout << "from A struct..." << "\n";
+  }
+};
+
+struct B
+{
+  void update()
+  {
+    std::cout << "from B struct..." << "\n";
+  }
+};
+
+int main()
+{
   playground pg{};
   pg.show_ABC_with_range();
 
@@ -26,4 +45,40 @@ int main() {
   std::cout << ss.truth << "\n";
   std::cout << ss.digit << "\n";
   std::cout << ss.sign << "\n";
+
+  std::cout << "\n---" << "variant" << "\n";
+  using var = std::variant<A, B>;
+  std::vector<var> items;
+  items.emplace_back(A{});
+  items.emplace_back(B{});
+  items.emplace_back(B{});
+  items.emplace_back(B{});
+  items.emplace_back(A{});
+  items.emplace_back(A{});
+  items.emplace_back(A{});
+
+  auto call_update = [](auto &item)
+  { return item.update(); };
+
+  for (auto &item : items) {
+    std::visit(call_update, item);
+  }
+
+  std::cout << "\n---" << "another approach" << "\n";
+  A a_arr[]{A{}, A{}, A{}};
+  B b_arr[]{B{}, B{}, B{}};
+
+  const auto do_update = [](auto &arr)
+  {
+    for (auto &item : arr) {
+      item.update();
+    }
+  };
+
+  do_update(a_arr);
+  do_update(b_arr);
+
+
+  return 0;
+
 }
