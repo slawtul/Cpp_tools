0cfbec12723abf0739ea21fc0ebc7548566ac895
slawtul <slawtul@javablues.org>
2021-07-11 00:12:57 +0200

    Add: new stuff

--- playground/src/passing_params.cpp
+++ playground/src/passing_params.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
+#include <memory>
 
 struct Widget
 {
     int id;
 };
 
-void call_ptr_widget(Widget* widget)
+void call_PTR_widget(Widget* widget)
 {
     if (widget) {
         std::cout << "passing with pointer *" << "\n";
@@ -14,7 +15,7 @@ void call_ptr_widget(Widget* widget)
     }
 }
 
-void call_ref_widget(Widget& widget)
+void call_REF_widget(Widget& widget)
 {
     std::cout << "passing with reference &" << "\n";
     std::cout << widget.id << "\n";
@@ -23,13 +24,31 @@ void call_ref_widget(Widget& widget)
 
 int main()
 {
+    // PASSING STACKED OBJECT TO FUNCTION WITH * AND &
+    std::cout << "// PASSING STACKED OBJECT TO FUNCTION WITH * AND &" << "\n";
+
     Widget widget{};
-    call_ptr_widget(&widget);
+
+    call_PTR_widget(&widget);
     std::cout << widget.id << "\n";
 
     widget.id = 0; // reset
 
-    call_ref_widget(widget);
+    call_REF_widget(widget);
     std::cout << widget.id << "\n";
 
+
+
+    // PASSING HEAPED OBJECT TO FUNCTION WITH * AND &
+    std::cout << "// PASSING HEAPED OBJECT TO FUNCTION WITH * AND &" << "\n";
+
+    auto uptr_widget = std::make_unique<Widget>();
+
+    call_PTR_widget(uptr_widget.get()); // get() - give me a raw pointer
+    std::cout << uptr_widget->id << "\n";
+
+    uptr_widget->id = 0; // reset
+
+    call_REF_widget(*uptr_widget); // *uptr_widget - give me value
+    std::cout << uptr_widget->id << "\n";
 }
